e88d14ec9248e63701b117ea68aa2276e603ef81
SimeonAlexandrov <simeon.alexandrov17@gmail.com>
2018-07-01 18:16:06 +0300

    Added docs.

--- config.php
+++ config.php
@@ -1,15 +1,15 @@
 <?php
-    $config=array(
-        'DB_HOST' => 'php-app-db.mysql.database.azure.com',
-        'DB_USER' => 'phpappuser@php-app-db',
-        'DB_PASS' => 'MySQLAzure2018',
-        'DB_NAME' => 'questio'
-    );
+    // $config=array(
+    //     'DB_HOST' => 'php-app-db.mysql.database.azure.com',
+    //     'DB_USER' => 'phpappuser@php-app-db',
+    //     'DB_PASS' => 'MySQLAzure2018',
+    //     'DB_NAME' => 'questio'
+    // );
 
-// $config=array(
-//     'DB_HOST' => 'localhost',
-//     'DB_USER' => 'root',
-//     'DB_PASS' => '',
-//     'DB_NAME' => 'questio'
-// );
+$config=array(
+    'DB_HOST' => 'localhost',
+    'DB_USER' => 'root',
+    'DB_PASS' => '',
+    'DB_NAME' => 'questio'
+);
 ?>
